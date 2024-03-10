75434f696533a1fa1001e026b03bce396eb09a05
Maxence L <contact@mlebrun.fr>
2024-03-10 19:34:20 +0100

    Add the main code

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+fetch = require('node-fetch-commonjs');
+module.exports.sendMessage = async function (msg, user, apikey) {
+    var response = await fetch("https://smsapi.free-mobile.fr/sendmsg?user="+user+"&pass="+apikey+"&msg=" + msg);
+    const body = await response.text();
+    return JSON.parse(body);
+}
